44195ed84190ee170202357db8833492859d6d7a
Liang Shi <liang.shi@protonmail.com>
2019-08-11 15:09:00 +1000

    add rubocop file

--- env.zsh
+++ env.zsh
@@ -6,5 +6,5 @@ export PATH=$HOME/.composer/vendor/bin:$PATH
 export FZF_DEFAULT_OPTS="--height 60% --layout=reverse --preview '(highlight -O ansi {} || cat {}) 2> /dev/null | head -500'"
 # export FZF_DEFAULT_COMMAND="fd --exclude={.git,.idea,.sass-cache,node_modules,build} --type f"
 
-. /usr/local/manual/z.sh
 eval "$(rbenv init -)"
+. /usr/local/manual/z.sh
